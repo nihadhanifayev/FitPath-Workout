d1f1c7f8cf78467f86b12b6e11aa36748185d24c
Nihad <nihad.hanifayev001@gmail.com>
2024-09-26 19:23:19 +0400

    Workout Fragment,Workout Adapter and Class,Workout Card design and design updates

--- app/src/main/java/com/example/fitpath/WorkoutsFragment.kt
+++ app/src/main/java/com/example/fitpath/WorkoutsFragment.kt
@@ -1,59 +1,32 @@
 package com.example.fitpath
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.fitpath.adapters.WorkoutsAdapter
+import com.example.fitpath.classes.Workout
+import com.example.fitpath.databinding.FragmentWorkoutsBinding
+import kotlinx.coroutines.currentCoroutineContext
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [WorkoutsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class WorkoutsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var design:FragmentWorkoutsBinding
+    private lateinit var adapter:WorkoutsAdapter
+    private lateinit var workouts:ArrayList<Workout>
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        design = FragmentWorkoutsBinding.inflate(inflater,container,false)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+
+        adapter = WorkoutsAdapter(requireContext(),workouts)
+        design.WorkoutRV.setHasFixedSize(true)
+        design.WorkoutRV.layoutManager = LinearLayoutManager(requireContext())
+        design.WorkoutRV.adapter = adapter
+        return design.root
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_workouts, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WorkoutsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WorkoutsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
---
+++ app/src/main/java/com/example/fitpath/classes/Workout.kt
@@ -0,0 +1,5 @@
+package com.example.fitpath.classes
+
+data class Workout(var workout:String,var workoutInfo:String,var workoutImage:String) {
+
+}
