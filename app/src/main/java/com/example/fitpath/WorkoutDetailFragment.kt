e49133c92f2dad140cc82a3ef254dad697aadca1
Nihad <nihad.hanifayev001@gmail.com>
2024-11-19 15:38:56 +0400

    Workout Detail Fragment Alternatives

--- app/src/main/java/com/example/fitpath/WorkoutDetailFragment.kt
+++ app/src/main/java/com/example/fitpath/WorkoutDetailFragment.kt
@@ -9,20 +9,41 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.fitpath.adapters.ChildRecyclerViewAdapter
+import com.example.fitpath.classes.ChildItem
+import com.example.fitpath.classes.Workout
 import com.example.fitpath.databinding.FragmentWorkoutDetailBinding
 import com.example.fitpath.models.WorkoutDetailFragmentViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class WorkoutDetailFragment : Fragment() {
     private lateinit var design:FragmentWorkoutDetailBinding
     private lateinit var viewmodel:WorkoutDetailFragmentViewModel
+    private lateinit var adapter:ChildRecyclerViewAdapter
+    private lateinit var ListWorkoutItem:ArrayList<ChildItem>
+    private lateinit var ListWorkout:ArrayList<Workout>
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         design = DataBindingUtil.inflate(inflater,R.layout.fragment_workout_detail,container,false)
         val bundle:WorkoutDetailFragmentArgs by navArgs()
         val workout = bundle.workout
         design.workout = workout
+        ListWorkoutItem = ArrayList<ChildItem>()
         design.imageViewWorkoutDetail.setImageResource((activity as AppCompatActivity).
         resources.getIdentifier(workout.workoutImage,"drawable",(activity as AppCompatActivity).packageName))
+        design.rvAlternatives.setHasFixedSize(true)
+        design.rvAlternatives.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL,false)
+        viewmodel.workoutLiveData.observe(viewLifecycleOwner){listWorkout->
+            ListWorkout = listWorkout as ArrayList<Workout>
+            for (childItem in ListWorkout){
+                val item = ChildItem(childItem.workout,childItem.workoutImage)
+                ListWorkoutItem.add(item)
+                adapter = ChildRecyclerViewAdapter(requireContext(),ListWorkoutItem)
+                design.adapter = adapter
+            }
+        }
+        viewmodel.getCategories(workout.workoutCategory)
         return design.root
     }
 
--- app/src/main/java/com/example/fitpath/models/WorkoutDetailFragmentViewModel.kt
+++ app/src/main/java/com/example/fitpath/models/WorkoutDetailFragmentViewModel.kt
@@ -1,6 +1,28 @@
 package com.example.fitpath.models
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.fitpath.classes.Workout
+import com.example.fitpath.classes.WorkoutCategory
+import com.example.fitpath.roomDB.dao.WorkoutDao
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class WorkoutDetailFragmentViewModel:ViewModel() {
+@HiltViewModel
+class WorkoutDetailFragmentViewModel @Inject constructor (var dao: WorkoutDao):ViewModel() {
+
+    var workoutLiveData=MutableLiveData<List<Workout>>()
+
+    init {
+        workoutLiveData=MutableLiveData<List<Workout>>()
+    }
+
+    fun getCategories(category:String){
+        val job = CoroutineScope(Dispatchers.Main).launch {
+            workoutLiveData.value = dao.getWorkouts(category)
+        }
+    }
 }
