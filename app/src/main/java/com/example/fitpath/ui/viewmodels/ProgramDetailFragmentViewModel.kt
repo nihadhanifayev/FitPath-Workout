344460d04da0fe9963256f033a361f884c40097b
nihad <nihad.hanifayev001@gmail.com>
2025-02-22 23:36:23 +0400

    Program Detail And Overview Fragment update

--- app/src/main/java/com/example/fitpath/ui/viewmodels/ProgramDetailFragmentViewModel.kt
+++ app/src/main/java/com/example/fitpath/ui/viewmodels/ProgramDetailFragmentViewModel.kt
@@ -2,6 +2,7 @@ package com.example.fitpath.ui.viewmodels
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.fitpath.data.model.Program
 import com.example.fitpath.data.model.ProgramWorkouts
 import com.example.fitpath.data.dao.ProgramDao
@@ -10,6 +11,7 @@ import com.example.fitpath.data.dao.WorkoutDao
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -21,50 +23,100 @@ class ProgramDetailFragmentViewModel @Inject constructor (var dao: ProgramDao, p
     var livedataLastID = MutableLiveData<Program>()
     var livedataLastProgramWorkouts = MutableLiveData<List<ProgramWorkouts>>()
 
+    private var selectedWorkoutsOne = ArrayList<String>()
+    private var selectedWorkoutsTwo = ArrayList<String>()
+    private var selectedWorkoutsThree = ArrayList<String>()
+    private var selectedWorkoutsFour = ArrayList<String>()
+    private var selectedWorkoutsFive = ArrayList<String>()
+    private var selectedWorkoutsSix = ArrayList<String>()
+    private var selectedWorkoutsSeven = ArrayList<String>()
+    var selectedWorkoutManager = ArrayList<ArrayList<String>>()
+    var workoutLists = ArrayList<ProgramWorkouts>()
+
     init {
         livedata = MutableLiveData<List<String>>()
         livedataLastID = MutableLiveData<Program>()
         livedataLastProgramWorkouts = MutableLiveData<List<ProgramWorkouts>>()
+        selectedWorkoutInit()
     }
+    private fun selectedWorkoutInit(){
+        selectedWorkoutManager.add(selectedWorkoutsOne)
+        selectedWorkoutManager.add(selectedWorkoutsTwo)
+        selectedWorkoutManager.add(selectedWorkoutsThree)
+        selectedWorkoutManager.add(selectedWorkoutsFour)
+        selectedWorkoutManager.add(selectedWorkoutsFive)
+        selectedWorkoutManager.add(selectedWorkoutsSix)
+        selectedWorkoutManager.add(selectedWorkoutsSeven)
+    }
+    fun workoutManagerClear(){
+        for (x in 0..6){
+            selectedWorkoutManager[x].clear()
+        }
+    }
+    fun observeAndAddSelectedWorkouts(workoutList:List<ProgramWorkouts>){
+        for (workout in workoutList){
+            if (workout.program_workout_day==1){selectedWorkoutManager[0].add(workout.workout_name)}
+            if (workout.program_workout_day==2){selectedWorkoutManager[1].add(workout.workout_name)}
+            if (workout.program_workout_day==3){selectedWorkoutManager[2].add(workout.workout_name)}
+            if (workout.program_workout_day==4){selectedWorkoutManager[3].add(workout.workout_name)}
+            if (workout.program_workout_day==5){selectedWorkoutManager[4].add(workout.workout_name)}
+            if (workout.program_workout_day==6){selectedWorkoutManager[5].add(workout.workout_name)}
+            if (workout.program_workout_day==7){selectedWorkoutManager[6].add(workout.workout_name)}
 
-    fun createProgram(program_title:String,dayOne:String,dayTwo:String,dayThree:String,dayFour:String,dayFife:String,daySix:String,daySeven:String,){
-        val job = CoroutineScope(Dispatchers.Main).launch {
-            val program_name = program_title
-            val day1 = dayOne
-            val day2 = dayTwo
-            val day3 = dayThree
-            val day4 = dayFour
-            val day5 = dayFife
-            val day6 = daySix
-            val day7 = daySeven
-            val program1 = Program(0,program_name,day1,day2,day3,day4,day5,day6,day7)
+        }
+    }
+    fun createProgram(programTitle:String, dayOne:String, dayTwo:String, dayThree:String, dayFour:String, dayFife:String, daySix:String, daySeven:String,){
+        viewModelScope.launch {
+            val program1 =
+                Program(
+                    0,
+                    programTitle,
+                    dayOne,
+                    dayTwo,
+                    dayThree,
+                    dayFour,
+                    dayFife,
+                    daySix,
+                    daySeven
+                )
             dao.addProgram(program1)
         }
     }
+
+    fun deleteSelectedWorkouts(number:Int){
+        when(number){
+            1 -> selectedWorkoutManager[0].clear()
+            2 -> selectedWorkoutManager[1].clear()
+            3 -> selectedWorkoutManager[2].clear()
+            4 -> selectedWorkoutManager[3].clear()
+            5 -> selectedWorkoutManager[4].clear()
+            6 -> selectedWorkoutManager[5].clear()
+            7 -> selectedWorkoutManager[6].clear()
+        }
+    }
+
     fun allWorkouts(){
-        val job = CoroutineScope(Dispatchers.Main).launch {
+        viewModelScope.launch {
             livedata.value = daoWorkout.getWorkoutsAll()
         }
     }
     fun addProgramWorkouts(program: ProgramWorkouts){
-        val job = CoroutineScope(Dispatchers.Main).launch {
+        viewModelScope.launch {
             daoProgramWorkouts.addProgramWorkout(program)
         }
     }
     fun getLastId(){
-        val job = CoroutineScope(Dispatchers.Main).launch {
+        viewModelScope.launch {
             livedataLastID.value = daoProgramWorkouts.getLastID()
         }
     }
     fun updateProgram(program: Program){
-        val job = CoroutineScope(Dispatchers.Main).launch {
+        viewModelScope.launch {
             dao.updateProgram(program)
         }
     }
-    fun getProgramWorkoutWithID(id:Int){
-        val job = CoroutineScope(Dispatchers.Main).launch{
-            livedataLastProgramWorkouts.value = daoProgramWorkouts.getProgramIDWorkoutsWithPW(id)
-        }
+    suspend fun getProgramWorkoutWithID(id:Int){
+        workoutLists = daoProgramWorkouts.getProgramIDWorkoutsWithPW(id) as ArrayList<ProgramWorkouts>
     }
 
 }
--- app/src/main/java/com/example/fitpath/ui/viewmodels/ProgramDetailsOverviewFragmentViewModel.kt
+++ app/src/main/java/com/example/fitpath/ui/viewmodels/ProgramDetailsOverviewFragmentViewModel.kt
@@ -1,72 +1,59 @@
 package com.example.fitpath.ui.viewmodels
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.fitpath.data.model.ProgramWorkouts
 import com.example.fitpath.data.dao.ProgramDao
 import com.example.fitpath.data.dao.ProgramWorkoutsDao
 import com.example.fitpath.data.dao.WorkoutDao
+import com.example.fitpath.data.model.ChildItem
+import com.example.fitpath.data.model.ParentItem
+import com.example.fitpath.data.model.Program
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ProgramDetailsOverviewFragmentViewModel @Inject constructor (var dao: ProgramDao, var daoWorkout: WorkoutDao, var daoProgramWorkouts: ProgramWorkoutsDao) :ViewModel() {
+    var childList1=ArrayList<ChildItem>()
+    var childList2=ArrayList<ChildItem>()
+    var childList3=ArrayList<ChildItem>()
+    var childList4=ArrayList<ChildItem>()
+    var childList5=ArrayList<ChildItem>()
+    var childList6=ArrayList<ChildItem>()
+    var childList7=ArrayList<ChildItem>()
+    var childListManager=ArrayList<ArrayList<ChildItem>>()
+    var programArray=ArrayList<ParentItem>()
 
-    var liveDataProgramWorkoutsone = MutableLiveData<List<ProgramWorkouts>>()
-    var liveDataProgramWorkoutstwo = MutableLiveData<List<ProgramWorkouts>>()
-    var liveDataProgramWorkoutsthree = MutableLiveData<List<ProgramWorkouts>>()
-    var liveDataProgramWorkoutsfour = MutableLiveData<List<ProgramWorkouts>>()
-    var liveDataProgramWorkoutsfife = MutableLiveData<List<ProgramWorkouts>>()
-    var liveDataProgramWorkoutssix = MutableLiveData<List<ProgramWorkouts>>()
-    var liveDataProgramWorkoutsseven = MutableLiveData<List<ProgramWorkouts>>()
-
+    var list = ArrayList<ProgramWorkouts>()
     init {
-        liveDataProgramWorkoutsone = MutableLiveData<List<ProgramWorkouts>>()
-        liveDataProgramWorkoutstwo = MutableLiveData<List<ProgramWorkouts>>()
-        liveDataProgramWorkoutsthree = MutableLiveData<List<ProgramWorkouts>>()
-        liveDataProgramWorkoutsfour = MutableLiveData<List<ProgramWorkouts>>()
-        liveDataProgramWorkoutsfife = MutableLiveData<List<ProgramWorkouts>>()
-        liveDataProgramWorkoutssix = MutableLiveData<List<ProgramWorkouts>>()
-        liveDataProgramWorkoutsseven = MutableLiveData<List<ProgramWorkouts>>()
+        childListManagerCollection()
     }
-
-    fun getProgramWorkoutsWithIdOne(id:Int,day:Int){
-        val job = CoroutineScope(Dispatchers.Main).launch {
-            liveDataProgramWorkoutsone.value = daoProgramWorkouts.getProgramIDWorkouts(id,day)
-        }
+    suspend fun getProgramWorkoutsWithId(id:Int,day: Int){
+        list = daoProgramWorkouts.getProgramIDWorkouts(id,day) as ArrayList<ProgramWorkouts>
     }
-    fun getProgramWorkoutsWithIdTwo(id:Int,day:Int){
-        val job = CoroutineScope(Dispatchers.Main).launch {
-            liveDataProgramWorkoutstwo.value = daoProgramWorkouts.getProgramIDWorkouts(id,day)
-        }
+    private fun childListManagerCollection(){
+        childListManager.add(childList1)
+        childListManager.add(childList2)
+        childListManager.add(childList3)
+        childListManager.add(childList4)
+        childListManager.add(childList5)
+        childListManager.add(childList6)
+        childListManager.add(childList7)
     }
-    fun getProgramWorkoutsWithIdThree(id:Int,day:Int){
-        val job = CoroutineScope(Dispatchers.Main).launch {
-            liveDataProgramWorkoutsthree.value = daoProgramWorkouts.getProgramIDWorkouts(id,day)
-        }
-    }
-    fun getProgramWorkoutsWithIdFour(id:Int,day:Int){
-        val job = CoroutineScope(Dispatchers.Main).launch {
-            liveDataProgramWorkoutsfour.value = daoProgramWorkouts.getProgramIDWorkouts(id,day)
-        }
-    }
-    fun getProgramWorkoutsWithIdFife(id:Int,day:Int){
-        val job = CoroutineScope(Dispatchers.Main).launch {
-            liveDataProgramWorkoutsfife.value = daoProgramWorkouts.getProgramIDWorkouts(id,day)
-        }
-    }
-    fun getProgramWorkoutsWithIdSix(id:Int,day:Int){
-        val job = CoroutineScope(Dispatchers.Main).launch {
-            liveDataProgramWorkoutssix.value = daoProgramWorkouts.getProgramIDWorkouts(id,day)
-        }
-    }
-    fun getProgramWorkoutsWithIdSeven(id:Int,day:Int){
-        val job = CoroutineScope(Dispatchers.Main).launch {
-            liveDataProgramWorkoutsseven.value = daoProgramWorkouts.getProgramIDWorkouts(id,day)
-        }
+    fun parentItemCollection(program:Program){
+        val parentItem1 = ParentItem(program.dayOne,childList1,false)
+        val parentItem2 = ParentItem(program.dayTwo,childList2,false)
+        val parentItem3 = ParentItem(program.dayThree,childList3,false)
+        val parentItem4 = ParentItem(program.dayFour,childList4,false)
+        val parentItem5 = ParentItem(program.dayFife,childList5,false)
+        val parentItem6 = ParentItem(program.daySix,childList6,false)
+        val parentItem7 = ParentItem(program.daySeven,childList7,false)
+        programArray.add(parentItem1)
+        programArray.add(parentItem2)
+        programArray.add(parentItem3)
+        programArray.add(parentItem4)
+        programArray.add(parentItem5)
+        programArray.add(parentItem6)
+        programArray.add(parentItem7)
     }
 
 
